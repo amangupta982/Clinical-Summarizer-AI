9375b74dfd8780adb4449c1551c524c53be1ef2d
Aman Gupta <amanmacair98@gmail.com>
2026-03-16 22:22:48 +0530

    the automatic created pycache file

--- testing/tests/test_app.py
+++ testing/tests/test_app.py
@@ -8,8 +8,7 @@ import numpy as np
 import sys
 import os
 
-sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
 from app import (
     process_clinical_text,
     analyze_sentiment,
