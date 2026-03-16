b5e0dde2177a14570aec4aab99ba28ec9a1f4efa
Aman Gupta <amanmacair98@gmail.com>
2026-03-16 22:47:54 +0530

    Manage d the folder structure

--- tests/test_app.py
+++ tests/test_app.py
@@ -8,7 +8,8 @@ import numpy as np
 import sys
 import os
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from app import (
     process_clinical_text,
     analyze_sentiment,
